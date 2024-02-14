a6b3a6d8ffcaec72fe011c4ae0c1ff420d6d3e9e
LokmanHossen <lokmanhossen230@gmail.com>
2024-02-15 02:42:12 +0600

    Add favorite

--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -1,20 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shop_app/providers/product.dart';
 
 import '../screen/product_details_screen.dart';
 
 class ProductItem extends StatelessWidget {
-  final String id;
-  final String title;
-  final String imageUrl;
+  const ProductItem({super.key});
 
-  const ProductItem(
-      {super.key,
-      required this.id,
-      required this.title,
-      required this.imageUrl});
+  // final String id;
+  // final String title;
+  // final String imageUrl;
+
+  // const ProductItem(
+  //     {super.key,
+  //     required this.id,
+  //     required this.title,
+  //     required this.imageUrl});
 
   @override
   Widget build(BuildContext context) {
+    final product = Provider.of<Product>(context);
     return ClipRRect(
       borderRadius: BorderRadius.circular(10),
       child: GridTile(
@@ -22,11 +27,14 @@ class ProductItem extends StatelessWidget {
           backgroundColor: Colors.black87,
           leading: IconButton(
             color: Theme.of(context).colorScheme.secondary,
-            icon: const Icon(Icons.favorite),
-            onPressed: () {},
+            icon: Icon(
+                product.isFavorite ? Icons.favorite : Icons.favorite_border),
+            onPressed: () {
+              product.toggleFavoriteStatus();
+            },
           ),
           title: Text(
-            title,
+            product.title,
             textAlign: TextAlign.center,
           ),
           trailing: IconButton(
@@ -39,7 +47,7 @@ class ProductItem extends StatelessWidget {
           onTap: () {
             Navigator.of(context).pushNamed(
               ProductDetailsScreen.routeName,
-              arguments: id,
+              arguments: product.id,
               // MaterialPageRoute(
               //   builder: (context) => ProductDetailsScreen(
               //     title: title,
@@ -48,7 +56,7 @@ class ProductItem extends StatelessWidget {
             );
           },
           child: Image.network(
-            imageUrl,
+            product.imageUrl,
             fit: BoxFit.cover,
           ),
         ),
