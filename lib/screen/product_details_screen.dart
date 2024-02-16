b5ff38b3537176dab3ae49aa34f1e47bcd699764
LokmanHossen <lokmanhossen230@gmail.com>
2024-02-17 02:40:59 +0600

    Product_Details

--- lib/screen/product_details_screen.dart
+++ lib/screen/product_details_screen.dart
@@ -23,6 +23,38 @@ class ProductDetailsScreen extends StatelessWidget {
         title: Text(loadedProduct.title),
         backgroundColor: Theme.of(context).primaryColor,
       ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              height: 300,
+              width: double.infinity,
+              child: Image.network(
+                loadedProduct.imageUrl,
+                fit: BoxFit.cover,
+              ),
+            ),
+            const SizedBox(height: 10),
+            Text(
+              '৳${loadedProduct.price}',
+              style: const TextStyle(
+                color: Colors.grey,
+                fontSize: 20,
+              ),
+            ),
+            const SizedBox(height: 10),
+            Container(
+              padding: const EdgeInsets.symmetric(horizontal: 10,),
+              width: double.infinity,
+              child: Text(
+                loadedProduct.description,
+                textAlign: TextAlign.center,
+                softWrap: true,
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
