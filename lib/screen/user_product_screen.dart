79d7cfb7209f6ee0590be64be4c6d431771f88fe
LokmanHossen <lokmanhossen230@gmail.com>
2024-02-18 02:44:34 +0600

    RouteEditProductScreen

--- lib/screen/user_product_screen.dart
+++ lib/screen/user_product_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:shop_app/screen/edit_product_screen.dart';
 import 'package:shop_app/widgets/app_drawer.dart';
 import 'package:shop_app/widgets/user_product_item.dart';
 import '../providers/products_provider.dart';
@@ -19,7 +20,7 @@ class UserProductScreen extends StatelessWidget {
           IconButton(
             icon: const Icon(Icons.add),
             onPressed: () {
-              //.....
+              Navigator.of(context).pushNamed(EditProductScreen.routeName);
             },
           ),
         ],
