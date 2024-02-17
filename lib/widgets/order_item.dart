749f1288b1f79296f4ea4bc8354ecbe57994d38b
LokmanHossen <lokmanhossen230@gmail.com>
2024-02-17 21:10:28 +0600

    Add OrderListCart

--- lib/widgets/order_item.dart
+++ lib/widgets/order_item.dart
@@ -1,12 +1,20 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:shop_app/providers/orders.dart' as ord;
 import 'package:intl/intl.dart';
 
-class OrderItem extends StatelessWidget {
+class OrderItem extends StatefulWidget {
   final ord.OrderItem order;
 
   const OrderItem({super.key, required this.order});
 
+  @override
+  State<OrderItem> createState() => _OrderItemState();
+}
+
+class _OrderItemState extends State<OrderItem> {
+  var _expanded = false;
   @override
   Widget build(BuildContext context) {
     return Card(
@@ -14,15 +22,46 @@ class OrderItem extends StatelessWidget {
       child: Column(
         children: [
           ListTile(
-            title: Text('৳${order.amount}'),
+            title: Text('৳${widget.order.amount}'),
             subtitle: Text(
-              DateFormat('dd/MM/yyyy hh:mm:ss').format(order.dateTime),
+              DateFormat('dd/MM/yyyy hh:mm:ss').format(widget.order.dateTime),
             ),
             trailing: IconButton(
-              onPressed: () {},
-              icon: const Icon(Icons.expand_more),
+              icon: Icon(_expanded ? Icons.expand_less : Icons.expand_more),
+              onPressed: () {
+                setState(
+                  () {
+                    _expanded = !_expanded;
+                  },
+                );
+              },
             ),
-          )
+          ),
+          if (_expanded)
+            Container(
+              padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 4),
+              height: min(widget.order.product.length * 20.0 + 10, 100),
+              child: ListView(
+                  children: widget.order.product
+                      .map(
+                        (prod) => Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text(
+                              prod.title,
+                              style: const TextStyle(
+                                fontSize: 16,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            // const SizedBox(width: 10),
+                            Text(
+                                '${prod.quantity} x ${prod.price.toStringAsFixed(2)}৳')
+                          ],
+                        ),
+                      )
+                      .toList()),
+            ),
         ],
       ),
     );
