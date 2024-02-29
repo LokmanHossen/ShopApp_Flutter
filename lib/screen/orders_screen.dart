2a1937f5cfc93b95f73bcf95cd31e34f8501dcfc
LokmanHossen <lokmanhossen230@gmail.com>
2024-03-01 03:27:40 +0600

    Using FutureBuilder widhets

--- lib/screen/orders_screen.dart
+++ lib/screen/orders_screen.dart
@@ -5,50 +5,69 @@ import 'package:shop_app/widgets/order_item.dart';
 
 import '../widgets/app_drawer.dart';
 
-class OrdersScreen extends StatefulWidget {
+class OrdersScreen extends StatelessWidget {
   const OrdersScreen({super.key});
 
   static const routeName = '/orders';
 
-  @override
-  State<OrdersScreen> createState() => _OrdersScreenState();
-}
+//   @override
+//   State<OrdersScreen> createState() => _OrdersScreenState();
+// }
 
-class _OrdersScreenState extends State<OrdersScreen> {
-  var _isLoading = false;
-  @override
-  void initState() {
-    Future.delayed(Duration.zero).then((_) async {
-      setState(() {
-        _isLoading = true;
-      });
-      await Provider.of<Orders>(context, listen: false).fetchAndSetOrders();
-      setState(() {
-        _isLoading = false;
-      });
-    });
-    super.initState();
-  }
+// class _OrdersScreenState extends State<OrdersScreen> {
+  // var _isLoading = false;
+  // @override
+  // void initState() {
+    // Future.delayed(Duration.zero).then((_) async {
+    // setState(() {
+    // _isLoading = true;
+    // // });
+    // Provider.of<Orders>(context, listen: false).fetchAndSetOrders().then((_) {
+    //   setState(() {
+    //     _isLoading = false;
+    //   });
+    // });
+
+    // });
+  //   super.initState();
+  // }
 
   @override
   Widget build(BuildContext context) {
-    final orderData = Provider.of<Orders>(context);
+    print('Builders orders');
+    // final orderData = Provider.of<Orders>(context);
     return Scaffold(
       appBar: AppBar(
         title: const Text('Your Orders'),
         backgroundColor: Theme.of(context).primaryColor,
       ),
       drawer: const AppDrawer(),
-      body: _isLoading
-          ? Center(
-              child: CircularProgressIndicator(
-              color: Theme.of(context).colorScheme.secondary,
-            ))
-          : ListView.builder(
-              itemCount: orderData.orders.length,
-              itemBuilder: (context, index) =>
-                  OrderItem(order: orderData.orders[index]),
-            ),
+      body: FutureBuilder(
+        future: Provider.of<Orders>(context, listen: false).fetchAndSetOrders(),
+        builder: (ctx, dataSnapshot) {
+          if (dataSnapshot.connectionState == ConnectionState.waiting) {
+            return Center(
+                child: CircularProgressIndicator(
+                    color: Theme.of(context).colorScheme.secondary));
+          } else {
+            if (dataSnapshot.error != null) {
+              //..........
+              //
+              return const Center(
+                child: Text('An error occccures!'),
+              );
+            } else {
+              return Consumer<Orders>(
+                builder: (context, orderData, child) => ListView.builder(
+                  itemCount: orderData.orders.length,
+                  itemBuilder: (context, index) =>
+                      OrderItem(order: orderData.orders[index]),
+                ),
+              );
+            }
+          }
+        },
+      ),
     );
   }
 }
