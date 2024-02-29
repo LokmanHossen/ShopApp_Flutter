f1a002862153dd84dcca9e37547c2b650981758c
LokmanHossen <lokmanhossen230@gmail.com>
2024-03-01 02:55:40 +0600

    Add Loading State

--- lib/screen/orders_screen.dart
+++ lib/screen/orders_screen.dart
@@ -5,10 +5,30 @@ import 'package:shop_app/widgets/order_item.dart';
 
 import '../widgets/app_drawer.dart';
 
-class OrdersScreen extends StatelessWidget {
+class OrdersScreen extends StatefulWidget {
   const OrdersScreen({super.key});
 
   static const routeName = '/orders';
+
+  @override
+  State<OrdersScreen> createState() => _OrdersScreenState();
+}
+
+class _OrdersScreenState extends State<OrdersScreen> {
+  var _isLoading = false;
+  @override
+  void initState() {
+    Future.delayed(Duration.zero).then((_) async {
+      setState(() {
+        _isLoading = true;
+      });
+      await Provider.of<Orders>(context, listen: false).fetchAndSetOrders();
+      setState(() {
+        _isLoading = false;
+      });
+    });
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -19,11 +39,16 @@ class OrdersScreen extends StatelessWidget {
         backgroundColor: Theme.of(context).primaryColor,
       ),
       drawer: const AppDrawer(),
-      body: ListView.builder(
-        itemCount: orderData.orders.length,
-        itemBuilder: (context, index) =>
-            OrderItem(order: orderData.orders[index]),
-      ),
+      body: _isLoading
+          ? Center(
+              child: CircularProgressIndicator(
+              color: Theme.of(context).colorScheme.secondary,
+            ))
+          : ListView.builder(
+              itemCount: orderData.orders.length,
+              itemBuilder: (context, index) =>
+                  OrderItem(order: orderData.orders[index]),
+            ),
     );
   }
 }
